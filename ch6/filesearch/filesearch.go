02e11735685fb73131194d58b0964b451033dc08
delmesia <wendellsmesia@gmail.com>
2024-10-02 02:57:38 +0800

    added ch6 filesearch

--- ch6/filesearch/filesearch.go
+++ ch6/filesearch/filesearch.go
@@ -8,20 +8,33 @@ import (
 	"sync"
 )
 
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go fileSearch(os.Args[1], os.Args[2], &wg)
+	wg.Wait()
+}
+
 func fileSearch(dir, filename string, wg *sync.WaitGroup) {
+	// Reads all files from the directory given to the function
 	files, err := os.ReadDir(dir)
 	if err != nil {
 		panic(err)
 	}
+
 	for _, file := range files {
+		// Joins each file to the directory. e.g 'cat.jpg' becomes /home/pics/cat.jpg
 		fpath := filepath.Join(dir, file.Name())
 		if strings.Contains(file.Name(), filename) {
 			fmt.Println(fpath)
 		}
+		// If it's a dir, adds 1 to the waitgroup before starting a new goroutine
 		if file.IsDir() {
 			wg.Add(1)
+			// Creates a goroutine recursively, searching in the new dir
 			go fileSearch(fpath, filename, wg)
 		}
 	}
+	// Marks Done() on the waitgroup after processing all files.
 	wg.Done()
 }
