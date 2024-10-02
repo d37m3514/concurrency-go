da84372df7f5514a826eb378fde48045eb9201f1
delmesia <wendellsmesia@gmail.com>
2024-10-03 07:08:21 +0800

    TODO: rewrite ch7

--- ch7/main.go
+++ ch7/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
 	msgChannel := make(chan int, 6)
 	var wg sync.WaitGroup
-	wg.Add(6)
+	wg.Add(1)
 	go receiver(msgChannel, &wg)
 	for i := 0; i <= 5; i++ {
 		msgChannel <- i
@@ -17,6 +18,7 @@ func main() {
 }
 func receiver(messages chan int, wg *sync.WaitGroup) {
 	for i := 0; i <= 5; i++ {
+		time.Sleep(1 * time.Second)
 		fmt.Println("Received:", <-messages)
 		wg.Done()
 	}
